459da1558250f50588d9490c17e12de0b5269774
sahillather002 <sahillather002@gmail.com>
2023-01-11 10:54:18 +0530

    es6 string template

--- Codes/6.js
+++ Codes/6.js
@@ -17,3 +17,34 @@ function makeGreeting(name , email, id){
 function makeGreeting(name , email, id){
     return `hello, ${name}. We've emailed you at ${email}.Your used id is '${id}'`;
 }
+
+// adding html elements / tags
+function makeGreetingTemplate(name,email,id) {
+    return(
+        "<div>" +
+        "<h1> Hello, " +
+        name + 
+        ".</h1>" +
+        "<p>We've emailed you at" +
+        email +
+        ". " +
+        "Your user id is '" +
+        id +
+        "'" +
+        "</p>" +
+        "<div>"
+    );
+}
+
+//ES6 template strings, multi line strings
+function makeGreetingTemplate(name,email,id){
+    return `
+        <div>
+            <h1>Hello , ${name}</h1>
+            <p>
+                we've emailed you at ${email},
+                Your user id is '${id}'.
+            </p>
+        </div>
+    `
+}
