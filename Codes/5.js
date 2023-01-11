b7ac1a21ab65e68997e7d4b1c930060b20e3e45f
sahillather002 <sahillather002@gmail.com>
2023-01-11 10:00:57 +0530

    js more basics

---
+++ Codes/5.js
@@ -0,0 +1,10 @@
+let anyVariable;
+console.log(anyVariable)//undefined
+// if we create a variable but not define it
+// is called a undefined
+
+const name = {
+    firstName:"Sudeep",
+    lastName:null
+}
+console.log(name)
