11264bfe7ade763b0789f89761fc8b4622613697
sahillather002 <sahillather002@gmail.com>
2023-01-11 10:42:41 +0530

    js string templet

--- Codes/6.js
+++ Codes/6.js
@@ -1,1 +1,12 @@
-// arrays in js
+// template string in js
+function makeGreeting(name , email, id){
+    return(
+        "hello, "+
+        name +
+        ". We've emailed you at " +
+        email +
+        "Your user id is '" +
+        id +
+        "'."
+    )
+}
