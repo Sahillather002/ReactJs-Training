15a72a827e943e7620c8adb3edaf9e829fa5f553
sahillather002 <sahillather002@gmail.com>
2023-01-11 10:06:15 +0530

    primitive and non primitve data types

--- Codes/5.js
+++ Codes/5.js
@@ -1,10 +1,15 @@
-let anyVariable;
-console.log(anyVariable)//undefined
-// if we create a variable but not define it
-// is called a undefined
-
-const name = {
-    firstName:"Sudeep",
-    lastName:null
+// primitive and non primitive data types
+// non primitive data types
+var personDetails = {
+    name:"Bill Gates",
+    age:17,
+    role:"Develper"
 }
-console.log(name)
+
+var secondPersonDetails = personDetails
+
+personDetails.name = "Elon musk"
+secondPersonDetails.age=54
+console.log(personDetails)
+console.log(secondPersonDetails)
+
