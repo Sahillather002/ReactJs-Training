d9597416597b76a3ca777eb82797d008d5c68452
sahillather002 <sahillather002@gmail.com>
2023-01-11 10:24:25 +0530

    working example

--- Codes/5.js
+++ Codes/5.js
@@ -21,13 +21,31 @@ function getDate(){
 }
 
 getDate();
-console.log(date)
+// console.log(date)
 //not availabe outsided func.
 // ReferenceError: date is not defined
 
 function getDate(){
     var date = new Date();
     function formatDate(){
-        
+        return date.toDateString()
     }
+    return formatDate();
 }
+
+function discountPrices(prices,discount){
+    var discounted = [];
+    for (var i=0;i<prices.length;i++){ // var is global scopped and these variables are available outside as well
+        var discountedPrice = prices[i] * (1 - discount)
+        var finalPrice = Math.round(discountedPrice * 100)/100
+        discounted.push(finalPrice)
+    }
+    // these are called temprary variables 
+    console.log(i)
+    console.log(discountedPrice)
+    console.log(finalPrice)
+    
+    return discounted;
+}
+
+console.log(discountPrices([100,250,350,140],0.5))
