28a8763c29fbca13be554ef8d352945cdf131b53
sahillather002 <sahillather002@gmail.com>
2023-01-11 08:28:06 +0530

    object simple example

--- Codes/3.js
+++ Codes/3.js
@@ -3,3 +3,7 @@
 // In JS , Objects may exist without classes
 // Objects are dynamic
 // i.e., we can add , delete , update at any time
+// add , delete , update methods at any time
+// no access control
+
+const account = {}; // it is an object
