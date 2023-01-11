6e33bf4fb6c980e96e11a771f564a5380aca6fd4
sahillather002 <sahillather002@gmail.com>
2023-01-11 08:26:39 +0530

    diff between objects for js and c++

--- Codes/3.js
+++ Codes/3.js
@@ -1,2 +1,5 @@
 // ***Objects in Javascript***
 // Everything in it is Javascript
+// In JS , Objects may exist without classes
+// Objects are dynamic
+// i.e., we can add , delete , update at any time
