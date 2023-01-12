00f8edc5290afc206b70e86cea45952da065c640
sahillather002 <sahillather002@gmail.com>
2023-01-12 10:44:08 +0530

    basic arrays intro complete

--- Codes/10.js
+++ Codes/10.js
@@ -9,7 +9,36 @@ console.log(typeof(numbers))
 
 // mutation
 
-const numbersBackward = numbers.reverse()
-console.log(numbersBackward)
-console.log(numbers)
+// const numbersBackward = numbers.reverse()
+// console.log(numbersBackward)
+// console.log(numbers)
 // it is mutable method as original is also changed
+
+const numbersBackward = [...numbers] // for read / copy
+console.log(numbers)
+console.log(numbersBackward)
+
+console.log(numbersBackward.reverse())
+
+numbers.push(7)
+console.log(numbers)
+numbers.pop(7)
+console.log(numbers)
+const aNum = [...numbers,7]
+console.log(aNum)
+numbers.unshift(0)
+console.log(numbers)
+console.log(numbers.slice(2,2))
+console.log(numbers.slice(2)) 
+console.log(numbers.slice(2,4))
+
+// slice creates new array it doesn't change original array
+// slice is imutable 
+console.log(numbers.splice(3,2))
+console.log(numbers)
+// splice is mutable as changes original array
+
+//array.map()
+//it retrn a new array by transforming the array by 
+//applying callback to each element of array
+//immutable
