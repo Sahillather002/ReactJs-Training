89ffbd87830c4089a749ce79133ce6f6ae9066e9
sahillather002 <sahillather002@gmail.com>
2023-01-12 10:06:25 +0530

    Extra codes and questions

--- Codes/10.js
+++ Codes/10.js
@@ -1,1 +1,15 @@
 // arrays in js
+let a1 = [] // empty array / array literal
+let a2 = Array() // empty array / array as function
+let a3 = new Array() // empty array / call to array constructor
+
+const numbers = [1,2,3,4,5,6]
+console.log(numbers)
+console.log(typeof(numbers))
+
+// mutation
+
+const numbersBackward = numbers.reverse()
+console.log(numbersBackward)
+console.log(numbers)
+// it is mutable method as original is also changed
