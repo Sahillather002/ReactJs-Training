3269364e896771a876063a2977365f49c8d1ab03
sahillather002 <sahillather002@gmail.com>
2023-01-12 08:35:06 +0530

    anonymus function starts

--- Codes/9.js
+++ Codes/9.js
@@ -19,3 +19,26 @@ function sumofArray(arr){ // return type not defined
 let sum = sumofArray([2,3,4,5,6])
 console.log(sum)
 
+// function sumOfNumbers() {
+//     console.log(arguments)
+// }
+
+// sumOfNumbers(2,3,4)
+// argument object will be returned
+
+function sumOfNumbers() {
+    let sum = 0;
+    for( let i=0;i<arguments.length;i++){
+        sum += arguments[i]
+    }
+    return sum
+}
+
+console.log(sumOfNumbers(2,3,4,5,6))
+// above function with unlimited number of arguments
+
+// Anonymous functions
+
+function(arr) {
+    
+}
