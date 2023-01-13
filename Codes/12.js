74432b81d7091dd0c3c83fcf55bd5e22c39a4dd9
sahillather002 <sahillather002@gmail.com>
2023-01-13 10:18:54 +0530

    npm examples

--- Codes/12.js
+++ Codes/12.js
@@ -3,3 +3,26 @@
 // version
 // major.minor.patch
 // version management
+
+<script src = "https://cdnjs.cloudflare.com/ajax/libs/react/18.2.0/umd/react.production.min.js">
+
+</script>
+
+/* problems with cdn traditional way of in culding packages in our application
+   cdn sever crashes
+   versioning problem
+   can't import react dom before react 
+   upload and download problem
+   dependency management problem
+   
+   Npm : 
+   Easy to upload/download packages
+   Easier to switch versions
+   Manage the dependencies
+   Do it all for free
+   
+   Acquired by github in 2018
+   A registry for hosting all the packages
+   CLI for accessing and downloading packages
+   
+*/
