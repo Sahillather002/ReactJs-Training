2c16f597c12500d6793b0c8c77ea7da8d92f3f2a
sahillather002 <sahillather002@gmail.com>
2023-01-13 09:23:05 +0530

    reduce function completes

--- Codes/10.js
+++ Codes/10.js
@@ -38,7 +38,7 @@ console.log(numbers.splice(3,2))
 console.log(numbers)
 // splice is mutable as changes original array
 
-//array.map()
+// *** array.map() ***
 //it retrn a new array by transforming the array by 
 //applying callback to each element of array
 //immutable
@@ -62,4 +62,63 @@ const countries = ["India","England","Iceland","Australia","Norway"]
 //     console.log(i,country.toUpperCase())
 // })
 
+// to reperesent it in single line
 countries.forEach((country,i) => console.log(i,country.toUpperCase()))
+
+// boths output:
+/* 
+0 INDIA
+1 ENGLAND
+2 ICELAND
+3 AUSTRALIA
+4 NORWAY
+*/
+
+// *** Filter ***
+// It filters out items based on some criteria
+// It loop through all the elements of the original array and
+// filter out the elements that matches the condition
+console.log(countries.filter(country => country.includes("land")))
+// filter is immutable as it does not modify orginal array
+const countriesWithoutLand = countries.filter(country => !country.includes("land"))
+console.log(countriesWithoutLand)
+
+const evenNumbers = numbers.filter(num => num%2==0)
+console.log(evenNumbers)
+
+const oddNumbers = numbers.filter(num => num%2!=0)
+console.log(oddNumbers)
+
+const personAges = [34,5,8,10,35,24,21,17,65,69,28]
+const filteredAges = personAges.filter((age) => age >= 18)
+console.log(filteredAges)
+
+
+// *** array.reduce() ***
+// It applies a reducer function to each and every element of the original array
+// and returns an output value
+ const number = [1,2,3,4,5,6]
+const sums = number.reduce((prev,curr) => prev+curr)
+console.log(sums)
+
+const product = number.reduce((acc,curt) => acc*curt)
+console.log(product)
+console.log("******************")
+const res = number.reduce((acc,curt) => {
+    console.log(acc);
+    console.log(curt);
+    return acc+curt;
+})
+
+const stringsArray = ["This","is","combined","reduce","function","!"]
+const combinedString = stringsArray.reduce((acc,curr) => acc+" "+curr)
+console.log(combinedString)
+
+const prod = numbers.reduce((acc,curr) => {
+    console.log("acc:"+acc);
+    console.log("curt"+curt);
+    return acc*curt
+})
+
+// *** find ***
+// it gives us first occurence of element in the array based on the condition
