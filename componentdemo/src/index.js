7cc297d021f65f28b1a17d6192bf6651e3d54710
sahillather002 <sahillather002@gmail.com>
2023-01-16 08:19:49 +0530

    simple react demo , deleting unecessary file

--- componentdemo/src/index.js
+++ componentdemo/src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <h1> No app.js</h1>
+    </div>
   </React.StrictMode>
 );
+
+// html element always start with lower case
+// where component of react starts with upper case
+// index.js is main entry file 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
