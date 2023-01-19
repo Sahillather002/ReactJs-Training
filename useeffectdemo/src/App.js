d2359f48b4687b1e21db075fb26bee8a49820b49
sahillather002 <sahillather002@gmail.com>
2023-01-19 09:53:26 +0530

    use effect

--- useeffectdemo/src/App.js
+++ useeffectdemo/src/App.js
@@ -2,18 +2,27 @@ import './App.css';
 import React, { useState} from 'react';
 function App() {
   const [users,setUsers]=useState([])
+  const [count,setCount]=useState(0)
   console.log("Component rendered")
 
   // put no dependency when you want to run it only once
+  // useEffect will run again if the count value changes
   React.useEffect(function(){
+    console.log("useeffect called")
     fetch("https://swapi.dev/api/people/")
   .then(res => res.json())
   .then(data => setUsers(data))
-},[])
+},[count])
+
+function handleClick(){
+  setCount(prevCount => prevCount + 1)
+}
 
   return (
     <div>
       <pre>{JSON.stringify(users,null,2)}</pre>
+      <h3>Count:{count}</h3>
+      <button onClick={handleClick}>Add</button>
     </div>
   );
 }
